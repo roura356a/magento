8fc6fccf6b298e99c86590463620867023e3d62a
Alberto <albert@studionone.com.au>
2018-05-22 14:46:28 +1000

    removed use of env vars in cron job

--- 2.1/scripts/cron.sh
+++ 2.1/scripts/cron.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-/usr/local/bin/php $MAGENTO_ROOT/bin/magento cron:run | grep -v "Ran jobs by schedule" >> $MAGENTO_ROOT/var/log/magento.cron.log
-/usr/local/bin/php $MAGENTO_ROOT/update/cron.php >> $MAGENTO_ROOT/var/log/update.cron.log
-/usr/local/bin/php $MAGENTO_ROOT/bin/magento setup:cron:run >> $MAGENTO_ROOT/var/log/setup.cron.log
+/usr/local/bin/php /var/www/html/bin/magento cron:run | grep -v "Ran jobs by schedule" >> /var/www/html/var/log/magento.cron.log
+/usr/local/bin/php /var/www/html/update/cron.php >> /var/www/html/var/log/update.cron.log
+/usr/local/bin/php /var/www/html/bin/magento setup:cron:run >> /var/www/html/var/log/setup.cron.log
 
 chown -R www-data:www-data app/design/frontend var pub

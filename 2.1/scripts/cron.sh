385aa6933471535314360fbe824266d9be47b3e0
Alberto <albert@studionone.com.au>
2018-05-22 15:44:28 +1000

    Updated cron job

--- 2.1/scripts/cron.sh
+++ 2.1/scripts/cron.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Running Magento Cron"
+
+echo "bin/magento cron:run"
 /usr/local/bin/php /var/www/html/bin/magento cron:run | grep -v "Ran jobs by schedule" >> /var/www/html/var/log/magento.cron.log
+
+echo "update/cron.php"
 /usr/local/bin/php /var/www/html/update/cron.php >> /var/www/html/var/log/update.cron.log
+
+echo "bin/magento setup:cron:run"
 /usr/local/bin/php /var/www/html/bin/magento setup:cron:run >> /var/www/html/var/log/setup.cron.log
 
-chown -R www-data:www-data app/design/frontend var pub
+echo "Setting permissions..."
+chown -R www-data:www-data /var/www/html/app/design/frontend /var/www/html/var /var/www/html/pub
+
+echo "Ran Magento cron job"
